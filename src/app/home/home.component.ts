80a07554fbbec72e2df2219c092341821372e9f4
Xesquim <matheus.g.cesquim@gmail.com>
2023-07-03 14:55:02 -0300

    Revert "Sharing response for the http request"
    This reverts commit 46913e63bdfdd670c6e52921b2c9f04a1c04a0cd.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Observable, noop } from "rxjs";
-import { map, shareReplay } from "rxjs/operators";
+import { map } from "rxjs/operators";
 import { createHttpObservable } from "../common/util";
 import { Course } from "../model/course";
 @Component({
@@ -17,9 +17,9 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     const http$ = createHttpObservable("/api/courses");
+
     const courses$: Observable<Course[]> = http$.pipe(
-      map((res) => res["payload"]),
-      shareReplay()
+      map((res) => res["payload"])
     );
 
     this.beginnersCourses$ = courses$.pipe(
